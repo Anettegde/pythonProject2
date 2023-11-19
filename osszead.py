f81db251ce389ef9b541b2154accd6e81dd64b6c
baget <timar.anett.gde@gmail.com>
2023-11-19 17:10:08 +0100

    ..

---
+++ osszead.py
@@ -0,0 +1,12 @@
+def osszeado(a, b):
+    return a + b
+
+
+print("Írj be egy számot:")
+szama = int(input())
+print("Írj be még egy számot:")
+szamb = int(input())
+
+print(f"a beírt eredmény: {osszeado(2, 3)}")
+
+print(f"a bekért eredmény: {osszeado(szama, szamb)}")
