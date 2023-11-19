06f08deffba9cec043d0ee54c356a54a64227cf6
baget <timar.anett.gde@gmail.com>
2023-11-19 16:26:36 +0100

    harmadik commit

--- gyumolcsok.py
+++ gyumolcsok.py
@@ -1,14 +1,15 @@
-gyumolcsok = ["korte" , "barack" , "ananász", "banán" , "alma"]
+gyumolcsok = ["korte", "barack", "ananász", "banán", "alma"]
 
 print(gyumolcsok)
-#sorbarendezés:
-x =sorted(gyumolcsok)
+# sorbarendezés:
+
+x = sorted(gyumolcsok)
 print(f"sorbarendezve:  {x} ")
 
 for gyumolcs in x:
-    print(gyumolcs);
+    print(gyumolcs)
 
-#"a"-val kezdődők:
-string ='a'
+# "a"-val kezdődők:
+string = 'a'
 avalkezdodolista = [item for item in gyumolcsok if item.startswith(string)]
 print(f"a-val kezdődök listája: {avalkezdodolista} startswith-el")
